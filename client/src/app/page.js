f2d3455e86384d49b8e51f1f47c4106ceb71735c
Cris3h <cris.treachi@gmail.com>
2023-12-31 02:04:45 -0300

    feature: amoxicillin

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,11 +1,34 @@
-import styles from './page.module.css'
-
+"use client";
+import { useState } from "react";
+import styles from "./page.module.css";
+import {amoxicillin} from "@/formulas";
 export default function Home() {
+  const [weight, setWeight] = useState();
+
+  const handleWeightInput = (e) => {
+    let result = amoxicillin(e.target.value)
+    setWeight(result)
+  };
+
   return (
     <main className={styles.main}>
-        <div className={styles}>
-          <h1>Hello world</h1>
-        </div>
+      <div className={styles}>
+        <h1>Dr Helper!</h1>
+        <aside> The best world wide pediatrician helper is here! / El mejor ayudante de los pediatras del mundo está acá!</aside>
+      </div>
+      <div>
+        
+        <aside> Amoxicillin: </aside>
+        <label>Insert weight / Ingrese peso </label>
+        <input
+          type="number"
+          name="weight"
+          onChange={(e) => handleWeightInput(e)}
+        />
+      </div>
+      <p>
+       Dosis: {weight}
+      </p>
     </main>
-  )
+  );
 }
---
+++ client/src/formulas.js
@@ -0,0 +1,8 @@
+//peso x 80 x 5  todo eso dividido 500 y el resultado dividido 3
+export const amoxicillin = (w) => {
+    let first = w * 80 * 5;
+    let second = first / 500
+    let final = second / 3
+
+    return final
+}
