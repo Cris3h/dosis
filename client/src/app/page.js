472e7fd9234edc43a91532ad15dd2914646608f3
Cris3h <cris.treachi@gmail.com>
2024-01-03 11:12:06 -0300

    feature: new ML dosis || add: styles

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -4,17 +4,28 @@ import styles from "./page.module.css";
 import { amoxicillin } from "@/formulas";
 
 export default function Home() {
-  const [weight, setWeight] = useState();
-  const [error, setError] = useState(); 
+  const [dosis, setDosis] = useState();
+  const [ml, setMl] = useState(250);
+  const [amoDosis, setAmoDosis] = useState();
 
-  const handleWeightInput = (e) => {
-    let result = amoxicillin(e.target.value);
-    e.target.value.length > 4 ? setWeight('wrong input!') : setWeight(result);
+  const handleInput = (e) => {
+    e.target.value?.lenght > 4
+      ? setDosis("wrong input!")
+      : setDosis(e.target.value);
+  };
+
+  const handleSelect = (ml) => {
+    setMl(ml.target.value);
+  };
+
+  const handleSubmit = () => {
+    const result = amoxicillin(ml, dosis);
+    setAmoDosis(result);
   };
 
   return (
     <main className={styles.main}>
-      <div className={styles}>
+      <div className={''}>
         <h1>Dr Helper!</h1>
         <aside>
           {" "}
@@ -22,16 +33,34 @@ export default function Home() {
           de los pediatras del mundo está acá!
         </aside>
       </div>
-      <div>
-        <aside> Amoxicillin: </aside>
-        <label>Insert weight / Ingrese peso </label>
-        <input
-          type="number"
-          name="weight"
-          onChange={(e) => handleWeightInput(e)}
-        />
+
+      <div style={{ display: "flex", justifyContent:'center', alignItems:'center', border:'solid', borderRadius: '8px', padding: '2rem', gap: '2rem'}}>
+        <div style={{flexDirection: 'row'}}>
+          <aside style={{marginBottom:'5px'}}> Amoxicillin: </aside>
+          <label style={{marginBottom:'5px'}}>Insert weight / Ingrese peso </label>
+          <input type="number" name="weight" onChange={(e) => handleInput(e)} style={{marginBottom:'5px'}}/>
+          <br />
+          <label style={{marginBottom:'5px'}}>ML: </label>
+          <select
+            name="ml"
+            id="amoxicillin"
+            onChange={(ml) => handleSelect(ml)}
+            style={{marginBottom:'5px'}}
+          >
+            <option value="250">250</option>
+            <option value="500">500</option>
+          </select>
+          <br />
+          <button type="button" onClick={() => handleSubmit()}
+          style={{marginBottom:'5px'}}>
+            consultar
+          </button>
+        </div>
+        <div style={{}}>
+          <p> Dosis: {amoDosis ? Math.floor(amoDosis[0]) : "0"}ml / 8hrs</p>
+          <p> {amoDosis ? Math.floor(amoDosis[1]) : "0"}ml / 12hrs</p>
+        </div>
       </div>
-      <p> Dosis: {weight}</p>
     </main>
   );
 }
--- client/src/formulas.js
+++ client/src/formulas.js
@@ -1,8 +1,17 @@
 //peso x 80 x 5  todo eso dividido 500 y el resultado dividido 3
-export const amoxicillin = (w) => {
+export const amoxicillin = (ml, w) => {
+  if (ml === '500') {
     let first = w * 80 * 5;
-    let second = first / 500
-    let final = second / 3
-
-    return final
-}
+    let second = first / 500;
+    let eight = second / 3;
+    let twelve = second / 2;
+    return [eight, twelve];
+  }
+  if (ml === '250') {
+    let first = w * 80 * 5;
+    let second = first / 250;
+    let eight = second / 3;
+    let twelve = second / 2;
+    return [eight, twelve];
+  }
+};
