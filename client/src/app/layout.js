e03bfe6ee2c74f6bd3383ae6bc9a7a50196f7125
Cris3h <cris.treachi@gmail.com>
2024-11-05 12:14:54 -0300

    ackee

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -66,6 +66,7 @@ export default function RootLayout({ children }) {
           content="https://raw.githubusercontent.com/Cris3h/dosis/main/client/src/app/favicon.ico"
         />
         <meta name="twitter:card" content="DrDosage - dosage helper" />
+        <script async src="https://ackee.fly.dev/tracker.js" data-ackee-server="https://ackee.fly.dev" data-ackee-domain-id="16fb5d57-3f82-4e5e-baf6-098f4af01f11"></script>
       </head>
       <body className={PatricHand.className}>{children}</body>
     </html>
