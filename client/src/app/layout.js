9fbe323b447bf50dbcd7b1255ea43bebdd3ad85e
Cris3h <cris.treachi@gmail.com>
2024-11-07 10:59:06 -0300

    ackee n

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -91,7 +91,7 @@ export default function RootLayout({ children }) {
           data-ackee-opts='{"ignoreLocalhost":false, "detailed": true}'
         ></script> */}
 
-<script async src="https://sh1cc4n7-3000.brs.devtunnels.ms/tracker.js/" data-ackee-server="https://sh1cc4n7-3000.brs.devtunnels.ms//" data-ackee-domain-id="enxjyFVlMl" data-ackee-opts='{"ignoreLocalhost":false, "detailed": true}'></script>
+<script async src="https://sh1cc4n7-3000.brs.devtunnels.ms/tracker.js" data-ackee-server="https://sh1cc4n7-3000.brs.devtunnels.ms" data-ackee-domain-id="enxjyFVlMl" data-ackee-opts='{"ignoreLocalhost":false, "detailed": true}'></script>
       </head>
       <body className={PatricHand.className}>{children}</body>
     </html>
